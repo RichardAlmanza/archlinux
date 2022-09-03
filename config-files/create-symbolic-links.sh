e74d0a8bb98bf6e4104832c05643cf776406c6b5
Richard Almanza <al.vegar@gmail.com>
2022-09-03 18:28:48 -0500

    oh-my-zsh: remove broken links in custom folder

--- config-files/create-symbolic-links.sh
+++ config-files/create-symbolic-links.sh
@@ -4,6 +4,12 @@ ZSH_CUSTOM=$ZSH/custom
 rm ~/.zshrc
 ln -s $PWD/oh-my-zsh/.zshrc  ~/.zshrc
 
+# remove broken links
+for file in $(find $ZSH_CUSTOM -xtype l)
+do
+    rm $file
+done
+
 # create links for zsh_custom
 for file in $PWD/oh-my-zsh/zsh_custom/*.zsh
 do
