1cbbc326f5cd436a247f481b07ab99a479106539
Richard Almanza <al.vegar@gmail.com>
2022-09-04 17:35:17 -0500

    oh-my-zsh: add aliases for lsd command

---
+++ config-files/oh-my-zsh/zsh_custom/extras.zsh
@@ -0,0 +1,9 @@
+# [ALIASES]
+# lsd
+alias l='lsd -ahl'
+alias la='lsd -Ahl'
+alias ll='lsd -hl'
+alias lz='l --directory-only --total-size'
+alias lt='l --tree'
+alias lta='la --tree'
+alias llt='ll --tree'
