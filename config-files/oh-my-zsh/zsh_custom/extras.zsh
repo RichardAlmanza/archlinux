b2f1617e19dc48e011d81d85e912aecaec284f94
Richard Almanza <al.vegar@gmail.com>
2022-09-30 18:00:09 -0500

    oh-my-zsh: fix lz alias

--- config-files/oh-my-zsh/zsh_custom/extras.zsh
+++ config-files/oh-my-zsh/zsh_custom/extras.zsh
@@ -3,7 +3,9 @@
 alias l='lsd -ahl'
 alias la='lsd -Ahl'
 alias ll='lsd -hl'
-alias lz='lsd -ahl --directory-only --total-size'
+alias lz='lsd -ahl --total-size'
 alias lt='lsd -ahl --tree'
 alias lta='lsd -Ahl --tree'
 alias llt='lsd -hl --tree'
+
+# [Extras]
