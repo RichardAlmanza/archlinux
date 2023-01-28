c7b26208ef3d509884fa3a592ed829c4e35188c2
Richard Almanza <al.vegar@gmail.com>
2023-01-27 21:37:07 -0500

    add exercism_actions, which call the exercism_actions.sh script

--- config-files/oh-my-zsh/zsh_custom/extras.zsh
+++ config-files/oh-my-zsh/zsh_custom/extras.zsh
@@ -8,4 +8,22 @@ alias lt='lsd -ahl --tree'
 alias lta='lsd -Ahl --tree'
 alias llt='lsd -hl --tree'
 
+# [FUNCTIONS]
+
+# needs Exercism and a configured --workspace; Git; VSCode
+# $1 Track eg. Go; $2 Exercise eg. need-for-speed
+function exercism_actions() {
+    action=$1
+    track=$2
+    exercise=$3
+
+    if [ $action = "start" ]; then
+        sh "$ZSH_CUSTOM/scripts/exercism_actions.sh" --start $track $exercise
+        return $?
+    elif [ $action = "finish" ]; then
+        sh "$ZSH_CUSTOM/scripts/exercism_actions.sh" --finish $track $exercise
+        return $?
+    fi
+
+}
 # [Extras]
