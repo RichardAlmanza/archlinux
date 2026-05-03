7dd188a78d8f42bb1b5d21626903adef9d9c4902
Richard Almanza <al.vegari@gmail.com>
2026-05-03 01:57:36 -0500

    Refac chat_ollama function to reuse its logic for other future service container

--- config-files/oh-my-zsh/zsh_custom/scripts/chat_ollama.sh
+++ config-files/oh-my-zsh/zsh_custom/scripts/chat_ollama.sh
@@ -9,55 +9,77 @@ get_containers_by_name() {
     "$CONTAINER_RUNNER" ps --quiet --filter name="$CONTAINER_NAME"
 }
 
+flipflop_container() {
+    CONTAINER_RUNNER="$1"
+    CONTAINER_NAME="$2"
+    CONTAINER_PROPERTIES="$3"
+    EXTRA_ACTION="${@:4}"
+
+    # Search container runner path
+    if [ "$CONTAINER_RUNNER" = "docker" ]; then
+        CONTAINER_RUNNER="$(command -v docker)"
+    elif [ "$CONTAINER_RUNNER" = "podman" ]; then
+        CONTAINER_RUNNER="$(command -v podman)"
+    elif [ "$CONTAINER_RUNNER" = "any" ]; then
+        { CONTAINER_RUNNER="$(command -v podman)"; } || \
+        { CONTAINER_RUNNER="$(command -v docker)"; }
+    else
+        echo "Invalid option for container runner: ${CONTAINER_RUNNER}"
+        exit 1
+    fi
+
+    # No container runner found
+    if [ "$CONTAINER_RUNNER" = "" ]; then
+        echo "No docker or podman found. Aborting"
+        exit 1
+    fi
+
+    # Is container running?
+    CONTAINER_ID="$(get_containers_by_name $CONTAINER_RUNNER $CONTAINER_NAME)"
+    CONTAINER_ID="$(echo "$CONTAINER_ID" | tr --squeeze-repeats "[:blank:]")"
+
+    # Stop running container
+    if [ "$CONTAINER_ID" != "" ]; then
+        echo "Stopping container ${CONTAINER_NAME}"
+        "$CONTAINER_RUNNER" container stop "$CONTAINER_NAME"
+
+    # Run container
+    else
+        echo "Using $(basename "$CONTAINER_RUNNER")"
+        echo "Starting temporal container $CONTAINER_NAME"
+        # shellcheck disable=SC2086
+        "$CONTAINER_RUNNER" run \
+            --rm -d \
+            --name "$CONTAINER_NAME" \
+            $CONTAINER_PROPERTIES && \
+        $EXTRA_ACTION
+    fi
+
+}
+
 chat_ollama() {
-    CONTAINER_RUNNER=""
     OLLAMA_CONTAINER_NAME="ollama-container"
     WEB_UI_CONTAINER_NAME="open-webui"
     OLLAMA_PORT=11434
     WEB_UI_PORT="${1:-3535}"
 
-    { CONTAINER_RUNNER="$(command -v podman)"; } || \
-    { CONTAINER_RUNNER="$(command -v docker)"; } || \
-    { echo "No docker or podman found. Aborting"; exit 1; }
-
-    CONTAINERS_RUNNING="\
-        $(get_containers_by_name $CONTAINER_RUNNER $OLLAMA_CONTAINER_NAME) \
-        $(get_containers_by_name $CONTAINER_RUNNER $WEB_UI_CONTAINER_NAME) \
-        "
-    CONTAINERS_RUNNING="$(echo "$CONTAINERS_RUNNING" | tr --squeeze-repeats "[:blank:]")"
-
-
-    if [ "$CONTAINERS_RUNNING" != " " ]; then
-        echo "Shutting down containers"
-
-        for CONTAINER_ID in ${CONTAINERS_RUNNING[@]}; do
-            echo "Stopping container ${CONTAINER_ID}"
-            "$CONTAINER_RUNNER" container stop "$CONTAINER_ID"
-        done
-
-        exit 0
-    fi
-
-
-    echo "Using $CONTAINER_RUNNER"
-    echo "Starting container $OLLAMA_CONTAINER_NAME"
-    "$CONTAINER_RUNNER" run --rm -d \
-        -v ollama:/root/.ollama \
-        -p "$OLLAMA_PORT":11434 \
-        --gpus=all \
-        --name "$OLLAMA_CONTAINER_NAME" \
-        --replace \
-        docker.io/ollama/ollama && \
-    echo "Starting container $WEB_UI_CONTAINER_NAME" && \
-    "$CONTAINER_RUNNER" run --rm -d \
-        -p "$WEB_UI_PORT":8080 \
-        -e OLLAMA_BASE_URL=http://localhost:"$OLLAMA_PORT" \
-        -v open-webui:/app/backend/data \
-        --name "$WEB_UI_CONTAINER_NAME" \
-        --network=pasta:-T,11434 \
-        --replace \
-        ghcr.io/open-webui/open-webui:main && \
-    echo "Open WebUI running at http://localhost:${WEB_UI_PORT}"
+    flipflop_container any \
+        "$OLLAMA_CONTAINER_NAME" \
+        "-v ollama:/root/.ollama \
+            -p ${OLLAMA_PORT}:11434 \
+            --gpus=all \
+            --replace \
+            docker.io/ollama/ollama" \
+        && \
+    flipflop_container any \
+        "$WEB_UI_CONTAINER_NAME" \
+        "-p ${WEB_UI_PORT}:8080 \
+            -e OLLAMA_BASE_URL=http://localhost:${OLLAMA_PORT} \
+            -v open-webui:/app/backend/data \
+            --network=pasta:-T,11434 \
+            --replace \
+            ghcr.io/open-webui/open-webui:main" \
+        "echo Open WebUI running at http://127.0.0.1:${WEB_UI_PORT}"
 }
 
-chat_ollama "$1"
+chat_ollama "$@"
